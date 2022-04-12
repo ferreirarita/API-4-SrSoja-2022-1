5f91dabbc7e26dac14b9d534486c8a5dc042f95b
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-04-11 21:03:32 -0300

    change correct stylesheet

--- SpyLeaf/src/Styles/dark.js
+++ SpyLeaf/src/Styles/dark.js
@@ -2,27 +2,67 @@ import { StyleSheet } from "react-native";
 
 const dark = StyleSheet.create({
     backgroundColor: '#343434',
-    toolbarColor: '#424242',
-    Line: '#9A9A9A',
-   
-    card: {
-        ColorPrimary: '#535353',
-        ColorSecondary: '#474747',
-    },
 
+    toolbar:{
+        backgroundColor: '#343434'
+    },
+    
+    underline:{
+        backgroundColor: '#9A9A9A'
+    },
+    
+    card: {
+        CardHeader:{
+            width: 330,
+            height: 70,
+            backgroundColor: '#535353',
+            top: 30,
+        }
+    },
+    
+    table:{
+        TablePrimary: {
+            alignItems: 'center',
+            width: '90%',
+            height: 440,
+            backgroundColor: '#535353',
+          },
+      
+        TableSecondary: {
+           width: '88%',
+           height: '90%',
+           backgroundColor: "#474747",
+          },
+    },
+    
     icon: {
         Color: '#C5C5C5',
     },
-
-    font: {
-        Family: 'Arial',
-        TitlePrimary: 17,
-        TitleSecondary: 11,
-        Inputs: 10,
-        ColorPrimary: '#C5C5C5',
-        ColorSecondary: '#BEBEBE',
+    
+    text: {
+        TitlePrimary:{
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: '#C5C5C5',
+            textAlign: 'center'
+        },
+    
+        TitleSecondary:{
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fontSize: 10,
+            color: '#C5C5C5',
+        },
+    
+        Inputs:{
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fontSize: 10,
+            color: '#BEBEBE',
+        }
     }
-
+    
 });
 
 export default dark
