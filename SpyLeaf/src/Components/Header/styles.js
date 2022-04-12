c2da2d6864f32655c0cb7c11a6184dd58cdc334b
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-04-12 00:51:46 -0300

    fix styles

---
+++ SpyLeaf/src/Components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+import light from "../../styles/light";
+
+const Header = styled.View`
+  background-color: #EE8600;
+  width: 100%;
+  height: 50px;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
+export { Header };
