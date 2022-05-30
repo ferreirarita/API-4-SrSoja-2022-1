ac868fc0ed4c8532e7cc8325a91d9972a3bb0ad3
Sarah781 <sarah.santana@fatec.sp.gov.br>
2022-05-30 09:51:54 -0300

    atualização 2

--- SrSoja/src/pages/Previsao_Tempo/index.js
+++ SrSoja/src/pages/Previsao_Tempo/index.js
@@ -2,29 +2,34 @@ import React, { useState } from 'react'
 import { View, Text, TextInput, Dimensions } from 'react-native'
 import styles from './styles'
 import moment from 'moment'
+import axios from 'axios'
 
 export default function Previsao_Tempo() {
 
     const [cityName, setCityName] = useState('')
 
-        return (
-            <View style={styles.container}>
- 
-                <View style={{justifyContent: 'center'}}>
-                    <TextInput 
-                        placeholder='Pesquise sua cidade'
-                        value={cityName}
-                        onChangeText={setCityName}
-                        style={{textAlign: "center"}}
-                    />
-                </View>
- 
-                <View>
-                    <Text style={{textAlign: "center"}}>
-                        {moment().format("DD/MM/YYYY")}
-                    </Text>
-                </View>
- 
+    // const getApiData = async function() {
+    //     return await axios.get("http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml")
+    // }
+
+    return (
+        <View style={styles.container}>
+
+            <View style={{justifyContent: 'center'}}>
+                <TextInput 
+                    placeholder='Pesquise sua cidade'
+                    value={cityName}
+                    onChangeText={setCityName}
+                    style={{textAlign: "center"}}
+                />
             </View>
-        );
+
+            <View>
+                <Text style={{textAlign: "center"}}>
+                    {moment().format("DD/MM/YYYY")}
+                </Text>
+            </View>
+
+        </View>
+    );
 }
