3225c8048dd460389d8c77ec7badc5cbf4b16dad
Matheus Emboaba <mateusemboabamelodesouza@hotmail.com>
2022-06-05 04:05:19 -0300

    TELA

--- SrSoja/src/pages/Previsao_Tempo/styles.js
+++ SrSoja/src/pages/Previsao_Tempo/styles.js
@@ -6,52 +6,6 @@ const styles = StyleSheet.create({
     flex: 1,
     ...stylesVar.background,
   },
-
-  header: {
-    flex: 0.1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    ...stylesVar.boxPrimary
-  },
-
-  headerColumn: {
-    flex: 1,
-  },
-
-  headerRow: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  headerButton: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    ...stylesVar.boxSecondary
-  },
-  headerText: {
-    color: '#343434',
-    fontWeight: "bold",
-    fontSize: 16,
-    letterSpacing: -1,
-  },
-
-  headerWeather: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    ...stylesVar.weather
-  },
-
-  headerWeatherText: {
-    color: '#E9E9E9',
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-
   body: {
     flex: 1,
     marginTop: 30,
@@ -80,25 +34,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center"
-  },
-  bodyRowDesabled: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  bodyButtonDesabled: {
-    flex: 1,
-    backgroundColor: "#E0E0E0",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 4,
-    margin: 4,
-  },
-  bodyTextDesabled: {
-    marginTop: 5,
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "#E0E0E0"
   },
 });
 export default styles;
