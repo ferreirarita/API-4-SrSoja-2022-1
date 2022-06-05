2b982c71593a86f8d630e47e7d0b32213d1269ff
Sarah6197 <fernandessarah.06@gmail.com>
2022-06-05 00:00:11 -0300

    fix floatingLabel 'WARNING'

--- SrSoja/src/index.js
+++ SrSoja/src/index.js
@@ -2,13 +2,14 @@ import 'react-native-gesture-handler';
 import React, {useEffect, useState, createContext} from "react";
 import { StatusBar } from 'react-native';
 import Pages from './pages';
+import { LogBox } from 'react-native';
 
 import openDatabase from './services/database/config';
 import {Context} from './context';
 
 
 export default function App () {
-
+  LogBox.ignoreLogs(["FloatingLabel"])
   useEffect(()=>{
     //openDatabase()//.then(response => setDatabase(response))
   },[])
